4a0abe0a00330d9b4697cadce9f10dbf358ccabe
Thomas Hirschberger <thomas.ch.hirschberger@gmx.de>
2023-05-28 10:15:01 +0200

    the streamlinkWrapper supports adding additional arguments as the vlc wrappers

--- scripts/streamlinkWrapper.bash
+++ scripts/streamlinkWrapper.bash
@@ -3,6 +3,20 @@ STREAM_TIMEOUT=$1
 SLEEP_TIME=$2
 LINK=$3
 
+if [ $# -lt 3 ]
+then
+	echo "Not enough arguments"
+	exit 1
+fi
+
+ADD_ARG_CNT=$(($#-3))
+if [ ${ADD_ARG_CNT} -gt 0 ]
+then
+	ADD_OPTS=${@:4:$ADD_ARG_CNT}
+else
+	ADD_OPTS=""
+fi
+
 STOP=0
 
 exit_script() {
@@ -15,7 +29,6 @@ trap exit_script exit
 
 
 while [ $STOP == 0 ] ; do 
-	streamlink "httpstream://${LINK}" live -p cvlc --retry-streams $STREAM_TIMEOUT
-	CUR_PID=$PID
+	streamlink "httpstream://${LINK}" live -p cvlc --retry-streams $STREAM_TIMEOUT ${ADD_OPTS}
 	sleep $SLEEP_TIME
 done
